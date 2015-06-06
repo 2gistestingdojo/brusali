d40b8aac0c8582e05a34fb71ec6fd6ba47a56f91
brusali <brusali@fake.com>
2015-06-06 13:06:04 +0600

    Release 3

--- csharp/TestingDojo2015/Release2AddTests.cs
+++ csharp/TestingDojo2015/Release2AddTests.cs
@@ -31,24 +31,52 @@ namespace TestingDojo2015
         [Test]
         public void AddItem()
         {
+            var productsList = this.MainWindowElement.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            var maxId = 0;
+
+            foreach (var item in productItems)
+            {
+                var tmp = Convert.ToInt32(item.FindElements(By.ClassName("TextBlock")).First().GetAttribute("Name"));
+
+                if (tmp > maxId)
+                {
+                    maxId = tmp;
+                }
+            }
+
             this.AddButtonElement.Click();
 
             var addWindow = this.Driver.FindElementById("AddNewProductWindow");
             var productName = addWindow.FindElement(By.Id("NameAW"));
             var addButton = addWindow.FindElement(By.Id("AddAW"));
+            var name = "Test product 1";
 
-            productName.SendKeys("Test product 1");
+            productName.SendKeys(name);
 
             addButton.Click();
 
-            var productsList = this.MainWindowElement.FindElement(By.Id("ProductsMW"));
-            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
-            var lastItem = productItems.Last();
-            var texts = lastItem.FindElements(By.ClassName("TextBlock"));
+            productsList = this.MainWindowElement.FindElement(By.Id("ProductsMW"));
+            productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            IWebElement element = null;
+         
+            foreach (var item in productItems)
+            {
+                var tmp = item.FindElements(By.ClassName("TextBlock")).Last().GetAttribute("Name");
+
+                if (tmp == name)
+                {
+                    element = item;
+                }
+            }
+
+            Assert.That(element, Is.Not.Null);
+
+            var texts = element.FindElements(By.ClassName("TextBlock"));
             var id = texts.First();
             var text = texts.Last();
 
-            Assert.That(id.GetAttribute("Name"), Is.EqualTo("9"));
+            Assert.That(id.GetAttribute("Name"), Is.EqualTo(Convert.ToString(maxId + 1)));
             Assert.That(text.GetAttribute("Name"), Is.EqualTo("Test product 1"));
         }
     }
---
+++ csharp/TestingDojo2015/Release3EditingAndSortingTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDojo2015
+{
+    using System.Threading;
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Interactions;
+
+    class Release3EditingAndSortingTests : BaseTestFixture
+    {
+        public IWebElement MainWindowElement;
+        public IWebElement SearchTextboxElement;
+        public IWebElement SearchButtonElement;
+
+        [SetUp]
+        public void SetUp()
+        {
+            base.SetUp();
+
+            this.MainWindowElement = this.Driver.FindElementById("MainWindow");
+            this.SearchTextboxElement = this.MainWindowElement.FindElement(By.Id("QueryMW"));
+            this.SearchButtonElement = this.MainWindowElement.FindElement(By.Id("SearchMW"));
+        }
+
+        [Test]
+        public void EditItem()
+        {
+            var productsList = this.MainWindowElement.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            var lastItem = productItems.Last();
+            var itemId = lastItem.FindElements(By.ClassName("TextBlock")).First().GetAttribute("Name");
+            var actions = new Actions(this.Driver);
+
+            actions.DoubleClick(lastItem).Perform();
+
+            var editWindow = this.Driver.FindElementById("ChangeProductWindow");
+            var productName = editWindow.FindElement(By.Id("NameCW"));
+            var saveButton = editWindow.FindElement(By.Id("SaveCW"));
+
+            productName.SendKeys("Asc sorting test product 1");
+
+            saveButton.Click();
+
+            productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            var firstItem = productItems.First();
+            var texts = firstItem.FindElements(By.ClassName("TextBlock"));
+            var id = texts.First();
+            var text = texts.Last();
+
+            Assert.That(text.GetAttribute("Name"), Is.EqualTo("Asc sorting test product 1"));
+            Assert.That(itemId, Is.EqualTo(id.GetAttribute("Name")));
+        }
+
+        [Test]
+        public void AddItemToBegin()
+        {
+            var addButtonElement = this.MainWindowElement.FindElement(By.Id("AddNewProductMW"));
+
+            addButtonElement.Click();
+
+            var addWindow = this.Driver.FindElementById("AddNewProductWindow");
+            var productName = addWindow.FindElement(By.Id("NameAW"));
+            var addButton = addWindow.FindElement(By.Id("AddAW"));
+
+            productName.SendKeys("Test product 1");
+
+            addButton.Click();
+
+            var productsList = this.MainWindowElement.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            var lastItem = productItems.First();
+            var texts = lastItem.FindElements(By.ClassName("TextBlock"));
+            var id = texts.First();
+            var text = texts.Last();
+
+            Assert.That(id.GetAttribute("Name"), Is.EqualTo("9"));
+            Assert.That(text.GetAttribute("Name"), Is.EqualTo("Test product 1"));
+        }
+    }
+}
